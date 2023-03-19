70435b9f137b4dc758f1f2d0b7f4195c0d29d4fd
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-03-19 20:26:22 +0100

    Added keybindings for lausnip

--- lua/key_bindings.lua
+++ lua/key_bindings.lua
@@ -1,3 +1,13 @@
 local opts = { noremap = true, silent = true }
 
 vim.keymap.set("n", "<C-r>", "<cmd>:RnvimrToggle<cr>", { desc = "[S]earch [R]esume" })
+
+vim.keymap.set({ "i", "s" }, "<Tab>", function()
+	if require("luasnip").choice_active() then
+		return "<Plug>luasnip-next-choice"
+	else
+		return "<Tab>"
+	end
+end, {
+	expr = true,
+})
