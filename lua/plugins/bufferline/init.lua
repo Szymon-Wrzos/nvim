a2de422548c1fa21fa6fc9c315cfe1c02f59af61
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-03-19 20:44:25 +0100

    Added keybindings to bufferline

--- lua/plugins/bufferline/init.lua
+++ lua/plugins/bufferline/init.lua
@@ -2,5 +2,11 @@ local status_ok, bufferline = pcall(require, "bufferline")
 if not status_ok then
 	return
 end
+local map = vim.api.nvim_set_keymap
+
+map("n", "<C-l>", "<cmd>:BufferLineCycleNext<cr>", { desc = "Next Tab" })
+map("n", "<C-k>", "<cmd>:BufferLineCyclePrev<cr>", { desc = "Previous Tab" })
+map("n", "<leader>l", "<cmd>:BufferLineCloseRight<cr>", { desc = "Close tabs to the right" })
+map("n", "<leader>k", "<cmd>:BufferLineCloseLeft<cr>", { desc = "Close tabs to the left" })
 
 bufferline.setup({})
