13f98bbd88b4a7f9059434cde1fa30d89df92374
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-03-28 21:18:57 +0200

    Lowered require to one

--- lua/plugins/cmp/init.lua
+++ lua/plugins/cmp/init.lua
@@ -1,15 +1,15 @@
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+
 local has_words_before = function()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-local cmp = require("cmp")
-local luasnip = require("luasnip")
-
 cmp.setup({
 	snippet = {
 		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
+			luasnip.lsp_expand(args.body)
 		end,
 	},
 	sources = cmp.config.sources({
