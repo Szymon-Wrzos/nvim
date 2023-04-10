4a55b772fc24ba3d8f2d1c8a0ca32fb3571b64f6
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-04-10 20:31:34 +0200

    Added keybinding for neogit

--- lua/key_bindings.lua
+++ lua/key_bindings.lua
@@ -4,6 +4,8 @@ vim.keymap.set("n", "<leader>ca", "<cmd>:CodeActionMenu<cr>", { desc = "[C]ode [
 
 vim.keymap.set("n", "<leader>rr", "<cmd>:NvimTreeToggle<cr>", { desc = "Nvim T[r]ee Toggle" })
 
+vim.keymap.set("n", "<leader>ng", "<cmd>:Neogit<cr>", { desc = "[N]eo[g]it" })
+
 vim.keymap.set({ "i", "s" }, "<Tab>", function()
 	if require("luasnip").choice_active() then
 		return "<Plug>luasnip-next-choice"
