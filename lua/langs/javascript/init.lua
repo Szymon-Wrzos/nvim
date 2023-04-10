c5ed6ec0c3ddc9648f69c9675c9ba19f8db13e8e
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-04-10 20:29:23 +0200

    Changed to prettierd

--- lua/langs/javascript/init.lua
+++ lua/langs/javascript/init.lua
@@ -24,7 +24,7 @@ local null_ls = require("null-ls")
 
 null_ls.register({
 	sources = {
-		null_ls.builtins.formatting.eslint_d,
+		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.code_actions.eslint_d,
 	},
