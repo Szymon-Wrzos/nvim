31e17110fb2581277177af36c4aa09cabf6c18a9
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-04-10 22:51:38 +0200

    Added explicit options

--- lua/plugins/gitsigns/init.lua
+++ lua/plugins/gitsigns/init.lua
@@ -1,1 +1,19 @@
-require("gitsigns").setup({})
+require("gitsigns").setup({
+	current_line_blame = true,
+	current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+		delay = 10,
+		ignore_whitespace = false,
+	},
+
+	signs = {
+		add = { text = "│" },
+		change = { text = "│" },
+		delete = { text = "_" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
+		untracked = { text = "┆" },
+	},
+})
