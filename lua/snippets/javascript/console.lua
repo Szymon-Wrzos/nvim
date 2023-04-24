49af2b90386c9be17671f13818b2e0f314e39f6a
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-04-24 07:16:59 +0200

    Fixed and refactoring

--- lua/snippets/javascript/console.lua
+++ lua/snippets/javascript/console.lua
@@ -1,37 +1,11 @@
 local luasnip = require("luasnip")
 local sn = luasnip.snippet
-local i = luasnip.insert_node
 local t = luasnip.text_node
-local c = luasnip.choice_node
 local s = luasnip.snippet_node
 local d = luasnip.dynamic_node
 local fmt = require("luasnip.extras.fmt").fmt
 local ts = require("vim.treesitter")
 local ts_query = require("vim.treesitter.query")
-
-local function dump(o)
-	if type(o) == "table" then
-		local ss = "{ "
-		for k, v in pairs(o) do
-			if type(k) ~= "number" then
-				k = '"' .. k .. '"'
-			end
-			ss = ss .. "[" .. k .. "] = " .. dump(v) .. ","
-		end
-		return ss .. "} "
-	else
-		return tostring(o)
-	end
-end
-
-local function contains(table, element)
-	for _, value in pairs(table) do
-		if value == element then
-			return true
-		end
-	end
-	return false
-end
 
 --- @param node TSNode | nil
 --- @returns TSNode | nil
@@ -41,13 +15,10 @@ local function seek_function_root(node, depth)
 	end
 
 	local node_type = node:type()
-	-- print(depth, node_type)
+	print(depth, node_type)
 	local js_function_type = { "function_declaration", "variable_declarator" }
-	if node_type == "program" then
-		return node
-	end
 
-	if contains(js_function_type, node_type) then
+	if vim.tbl_contains(js_function_type, node_type) then
 		return node
 	end
 
@@ -61,7 +32,9 @@ local function get_data(node)
 	if node == nil then
 		return nil
 	end
-	print(node:type())
+	if node:type() == "program" then
+		return "global"
+	end
 	local parsed_query = ts_query.parse("javascript", [[(identifier) @name]])
 	-- print("[get_data]")
 	for _, parsed_node, _ in parsed_query:iter_captures(node, 0, node:start(), node:end_()) do
@@ -75,7 +48,7 @@ local console = sn(
 		test = t(""),
 		function_name = d(1, function()
 			local function_root = seek_function_root(ts:get_node(), 0)
-			local data = get_data(function_root)
+			local data = function_root ~= nil and get_data(function_root) or ""
 			return s(nil, { t(data) })
 		end),
 	})
