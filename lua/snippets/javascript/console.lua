6ef4e4c59dc9f4a2ddd71725afe522673ed0d384
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-04-30 09:56:52 +0200

    console snippets supports now classes. Gotta de-hardcode it

--- lua/snippets/javascript/console.lua
+++ lua/snippets/javascript/console.lua
@@ -4,38 +4,44 @@ local t = luasnip.text_node
 local s = luasnip.snippet_node
 local d = luasnip.dynamic_node
 local fmt = require("luasnip.extras.fmt").fmt
-local ts = require("vim.treesitter")
-local ts_query = require("vim.treesitter.query")
+local ts = vim.treesitter
+local ts_query = ts.query
 
 --- @param node TSNode | nil
+--- @param ts_elements table<string>
 --- @returns TSNode | nil
-local function seek_function_root(node, depth)
+local function seek_function_root(node, ts_elements)
 	if node == nil then
 		return nil
 	end
-
 	local node_type = node:type()
-	print(depth, node_type)
-	local js_function_type = { "function_declaration", "variable_declarator" }
-
-	if vim.tbl_contains(js_function_type, node_type) then
+	if vim.tbl_contains(ts_elements, node_type) or node_type == "program" then
 		return node
 	end
 
 	local parent = node:parent()
 
-	return seek_function_root(parent, depth + 1)
+	return seek_function_root(parent, ts_elements)
 end
 
 --- @param node TSNode | nil
 local function get_data(node)
+	local filetype = vim.bo.filetype
+	local query_lang = filetype == "javascriptreact" and "jsx" or filetype
 	if node == nil then
 		return nil
 	end
-	if node:type() == "program" then
+	local node_type = node:type()
+	if node_type == "program" then
 		return "global"
 	end
-	local parsed_query = ts_query.parse("javascript", [[(identifier) @name]])
+	vim.print(node_type)
+	local parsed_query =
+		ts_query.parse(query_lang, (node_type == "method_definition" or node_type == "field_definition") and [[
+      (property_identifier) @name
+    ]] or [[
+      (identifier) @name
+    ]])
 	-- print("[get_data]")
 	for _, parsed_node, _ in parsed_query:iter_captures(node, 0, node:start(), node:end_()) do
 		return ts.get_node_text(parsed_node, 0)
@@ -47,7 +53,11 @@ local console = sn(
 	fmt([[ console.log("[{function_name}]","{test}")]], {
 		test = t(""),
 		function_name = d(1, function()
-			local function_root = seek_function_root(ts:get_node(), 0)
+			local ts_node = ts.get_node()
+			local function_root = seek_function_root(
+				ts_node,
+				{ "function_declaration", "variable_declarator", "method_definition", "field_definition" }
+			)
 			local data = function_root ~= nil and get_data(function_root) or ""
 			return s(nil, { t(data) })
 		end),
