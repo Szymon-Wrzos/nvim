b9ef02c5ba10215adfbb68aaaea2930a18df24eb
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-05-09 13:10:10 +0200

    Telescope shows hidden files save for dist, node_modules and .git

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -1,8 +1,20 @@
+local telescope = require("telescope")
 local builtin = require("telescope.builtin")
+
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>fj", builtin.lsp_references, {})
 vim.keymap.set("n", "<leader>fn", builtin.lsp_definitions, {})
-require("telescope").load_extension("luasnip")
+telescope.load_extension("luasnip")
+telescope.setup({
+	defaults = {
+		file_ignore_patterns = { ".git", "node_modules", "dist" },
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
+	},
+})
