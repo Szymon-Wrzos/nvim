17a9ca12974f285d928af679bda6e4ac4e9cb7da
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-05-09 13:09:28 +0200

    Removed "help"

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup({
 	-- A list of parser names, or "all" (the four listed parsers should always be installed)
-	ensure_installed = { "c", "vim", "help", "markdown", "markdown_inline" },
+	ensure_installed = { "c", "vim", "markdown", "markdown_inline" },
 	auto_install = true,
 	highlight = {
 		enable = true,
