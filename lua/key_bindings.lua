a32e006295aa58529d0ab1b83b6a422434b56290
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-05-31 22:06:52 +0200

    Changed command from Neogit to LazyGit

--- lua/key_bindings.lua
+++ lua/key_bindings.lua
@@ -4,7 +4,7 @@ vim.keymap.set("n", "<leader>ca", "<cmd>:CodeActionMenu<cr>", { desc = "[C]ode [
 
 vim.keymap.set("n", "<leader>rr", "<cmd>:NvimTreeToggle<cr>", { desc = "Nvim T[r]ee Toggle" })
 
-vim.keymap.set("n", "<leader>ng", "<cmd>:Neogit<cr>", { desc = "[N]eo[g]it" })
+vim.keymap.set("n", "<leader>ng", "<cmd>:LazyGit<cr>", { desc = "Lazy[g]it" })
 
 vim.keymap.set({ "i", "s" }, "<Tab>", function()
 	if require("luasnip").choice_active() then
