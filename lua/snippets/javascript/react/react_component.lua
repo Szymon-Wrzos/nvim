3c8a033d1893ba55d0c77e3654b9f97f11913a92
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-06-03 21:55:55 +0200

    Refactoring snippet to use repeat_duplicates

--- lua/snippets/javascript/react/react_component.lua
+++ lua/snippets/javascript/react/react_component.lua
@@ -2,7 +2,6 @@ local luasnip = require("luasnip")
 local sn = luasnip.snippet
 local i = luasnip.insert_node
 local c = luasnip.choice_node
-local t = luasnip.t
 local s = luasnip.snippet_node
 local d = luasnip.dynamic_node
 local fmt = require("luasnip.extras.fmt").fmt
@@ -17,20 +16,13 @@ import PropTypes from 'prop-types';
 const {name} = () => {{
   {}/* Your function goes here*/
 }};
-{prop_types}
+{name}.propTypes = {{}}
 
 {export_method}
 ]],
 		{
-			-- i(1) is at nodes[1], i(2) at nodes[2].
 			name = i(1, "Component"),
-			prop_types = d(2, function(args)
-				local component_name = args[1][1]
-				return s(nil, {
-					t(component_name .. ".propTypes = {};"),
-				})
-			end, { 1 }),
-			export_method = d(3, function(args)
+			export_method = d(2, function(args)
 				local component_name = args[1][1]
 				return s(nil, {
 					c(1, {
@@ -40,6 +32,9 @@ const {name} = () => {{
 				})
 			end, { 1 }),
 			i(0),
+		},
+		{
+			repeat_duplicates = true,
 		}
 	)
 )
