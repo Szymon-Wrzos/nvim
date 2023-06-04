0c12a3984f793e2366dfb6fe101c3c765affdce9
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-06-04 20:36:06 +0200

    Added LSP indicators to bufferline

--- lua/plugins/bufferline/init.lua
+++ lua/plugins/bufferline/init.lua
@@ -4,9 +4,19 @@ if not status_ok then
 end
 local map = vim.api.nvim_set_keymap
 
+local diagnostics_indicator = function(count, level, diagnostics_dict, context)
+	local icon = level:match("error") and " " or " "
+	return " " .. icon .. count
+end
+
 map("n", "<C-l>", "<cmd>:BufferLineCycleNext<cr>", { desc = "Next Tab" })
 map("n", "<C-k>", "<cmd>:BufferLineCyclePrev<cr>", { desc = "Previous Tab" })
 map("n", "<leader>l", "<cmd>:BufferLineCloseRight<cr>", { desc = "Close tabs to the right" })
 map("n", "<leader>k", "<cmd>:BufferLineCloseLeft<cr>", { desc = "Close tabs to the left" })
 
-bufferline.setup({})
+bufferline.setup({
+	options = {
+		diagnostics = "nvim_lsp",
+		diagnostics_indicator = diagnostics_indicator,
+	},
+})
