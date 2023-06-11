e6416d8741bba0eda4104e903a29713d08926a4a
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-06-11 21:18:25 +0200

    Added dap ui and made it work for python

--- init.lua
+++ init.lua
@@ -68,6 +68,7 @@ require("packer").startup(function(use)
 	use({ "romgrk/fzy-lua-native" })
 	use("mfussenegger/nvim-dap")
 	use("jay-babu/mason-nvim-dap.nvim")
+	use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } })
 end)
 
 require("key_bindings")
@@ -86,7 +87,7 @@ require("plugins.spectre.init")
 require("plugins.nvim_tree.init")
 require("plugins.codeium.init")
 require("plugins.wilder.init")
--- require("plugins.wilder.init")
+require("plugins.dap.init")
 -- Languages setup
 local null_ls = require("null-ls")
 null_ls.setup({
