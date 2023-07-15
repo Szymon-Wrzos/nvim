6ac8770ba46b8f2cd92c1acdb1f2c3167a45c694
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-15 23:40:15 +0200

    Used pack to remove redundant destructure

--- lua/snippets/javascript/console.lua
+++ lua/snippets/javascript/console.lua
@@ -5,6 +5,7 @@ local t = luasnip.text_node
 local s = luasnip.snippet_node
 local d = luasnip.dynamic_node
 local fmt = require("luasnip.extras.fmt").fmt
+
 --- @type TreesitterModule
 local ts = vim.treesitter
 local ts_query = ts.query
@@ -41,10 +42,7 @@ local function get_query_data()
 			},
 		}
 		for key, data in pairs(javascript_query_data) do
-			if not data[key] then
-				query_data[key] = data
-			else
-			end
+			query_data[key] = data
 		end
 	end
 
@@ -57,7 +55,7 @@ local function get_query_data()
 			full_query = full_query .. "(" .. key .. " (" .. inner_key .. ") " .. inner_val .. ")\n"
 		end
 	end
-	return breakpoints, full_query
+	return table.pack(breakpoints, full_query)
 end
 
 --- @param node TSNode | nil
@@ -87,7 +85,7 @@ local function get_data(node)
 	if node_type == "program" then
 		return "global"
 	end
-	local breakpoints, full_query = get_query_data()
+	local full_query = get_query_data()[2]
 	vim.print(full_query)
 	local parsed_query = ts_query.parse(vim.bo.filetype, full_query)
 	for _, parsed_node, _ in parsed_query:iter_captures(node, 0, node:start(), node:end_()) do
@@ -102,7 +100,7 @@ local console = sn(
 			local checked_value = args[1][1]
 			local ts_node = ts.get_node()
 
-			local breakpoints, full_query = get_query_data()
+			local breakpoints = get_query_data()[1]
 			local function_root = seek_function_root(ts_node, breakpoints)
 			local data = function_root ~= nil and get_data(function_root) or ""
 			local output = checked_value ~= "" and t(data .. " - " .. checked_value) or t(data)
