74ac0fa6be46d7a9c2477db9c0be99bd39732d4e
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-08-12 21:18:05 +0200

    Restricted volar to vue

--- lua/langs/vue/init.lua
+++ lua/langs/vue/init.lua
@@ -14,8 +14,10 @@ require("mason-null-ls").setup({
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+
 require("lspconfig")["volar"].setup({
 	capabilities = capabilities,
+	filetypes = {"vue" },
 })
 
 local null_ls = require("null-ls")
