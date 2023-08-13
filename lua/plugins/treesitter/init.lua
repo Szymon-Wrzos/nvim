54de52749a0a06bfa984b2f1ebd02f3114541507
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-08-13 23:42:03 +0200

    Removed highlight enable true

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -2,9 +2,6 @@ require("nvim-treesitter.configs").setup({
 	-- A list of parser names, or "all" (the four listed parsers should always be installed)
 	ensure_installed = { "c", "vim", "markdown", "markdown_inline" },
 	auto_install = true,
-	highlight = {
-		enable = true,
-	},
 	playground = {
 		enable = true,
 		disable = {},
