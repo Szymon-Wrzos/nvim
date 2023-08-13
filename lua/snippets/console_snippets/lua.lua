6a0d0783e987449a6971d4fafe5edd6824c61a86
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-08-13 19:02:20 +0200

    Seems to work now and it differs if it's in .config or not

--- lua/snippets/console_snippets/lua.lua
+++ lua/snippets/console_snippets/lua.lua
@@ -1,12 +1,26 @@
 local create_snippet = require("snippets.create_printing_snippet")
 
 local query = [[
-(assignment_statement (variable_list (identifier) @name))]]
+(assignment_statement (variable_list (identifier) @name))
+(function_declaration (identifier) @name)
+]]
 
 local breakpoints = {
 	"assignment_statement",
+	"function_declaration",
 }
 
-local snippet = create_snippet("lua", [[vim.print("{function_name}","{var}")]], query, breakpoints)
+local path = require("plenary.path")
+--- @type string
+local path_to_dir = path:new():parents()[1]
+
+local format = ""
+if path_to_dir:find(".config") ~= nil then
+	format = [[vim.print("{function_name}", {var})]]
+else
+	format = [[print("{function_name}", {var})]]
+end
+
+local snippet = create_snippet("lua", format, query, breakpoints)
 
 return snippet
