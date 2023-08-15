4b7065eec2d4a008d5e5a05b5a657239ca67599d
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-08-15 13:51:06 +0200

    Changed config of which_key window

--- lua/plugins/which_key/init.lua
+++ lua/plugins/which_key/init.lua
@@ -13,17 +13,17 @@ require("which-key").setup({
 		count = true,
 	},
 	window = {
-		border = "none", -- none, single, double, shadow
+		border = "single", -- none, single, double, shadow
 		position = "bottom", -- bottom, top
-		margin = { 1, 0, 0.5, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
-		padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-		winblend = 50, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+		margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
+		padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
+		winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
 		zindex = 1000, -- positive value to position WhichKey above other floating windows.
 	},
 	layout = {
 		height = { min = 4, max = 25 }, -- min and max height of the columns
-		width = { min = 10, max = 25 }, -- min and max width of the columns
+		width = { min = 4, max = 25 }, -- min and max width of the columns
 		spacing = 3, -- spacing between columns
-		align = "right", -- align columns left, center or right
+		align = "center", -- align columns left, center or right
 	},
 })
