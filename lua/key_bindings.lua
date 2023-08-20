69d652b70bb80634a882843f9837aa3042cf1426
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-08-20 23:07:15 +0200

    Removed reundant binding

--- lua/key_bindings.lua
+++ lua/key_bindings.lua
@@ -1,5 +1,3 @@
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set("n", "<leader>ca", "<cmd>:CodeActionMenu<cr>", { desc = "[C]ode [A]ction" })
-
 vim.keymap.set({ "n" }, "<leader>ha", vim.lsp.buf.hover, { desc = "Trigger [h]over [a]ction" })
