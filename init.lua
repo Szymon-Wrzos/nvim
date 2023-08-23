05d420434daf5d2aa2c5675c083a2490ac6ce666
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-08-23 23:46:42 +0200

    Removed null ls and require langs

--- init.lua
+++ init.lua
@@ -3,26 +3,3 @@ require("plugins_entry")
 require("key_bindings")
 require("opts")
 
--- Languages setup
-local null_ls = require("null-ls")
-null_ls.setup({
-	sources = {
-		null_ls.builtins.completion.luasnip,
-	},
-	on_attach = require("lsp-format").on_attach,
-})
-
-require("langs.lua.init")
-require("langs.rust.init")
-require("langs.javascript.init")
-require("langs.css.init")
-require("langs.html.init")
-require("langs.tailwindcss.init")
-require("langs.python.init")
-require("langs.json.init")
-require("langs.yaml.init")
-require("langs.markdown.init")
-require("langs.graphql.init")
-require("langs.vue.init")
-require("langs.sh.init")
-require("langs.dockerfile.init")
