bd94146b7e4a4ca9a450b54680991002fe798d9b
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-08-23 23:47:09 +0200

    Added constact with languages settings

---
+++ lua/utils/langs_table.lua
@@ -0,0 +1,17 @@
+local langs_table = {
+	lua = require("langs.lua.init"),
+	rust = require("langs.rust.init"),
+	js = require("langs.javascript.init"),
+	css = require("langs.css.init"),
+	html = require("langs.html.init"),
+	tailwindcss = require("langs.tailwindcss.init"),
+	python = require("langs.python.init"),
+	json = require("langs.json.init"),
+	yaml = require("langs.yaml.init"),
+	markdown = require("langs.markdown.init"),
+	graphql = require("langs.graphql.init"),
+	vue = require("langs.vue.init"),
+	sh = require("langs.sh.init"),
+	dockerfile = require("langs.dockerfile.init"),
+}
+return langs_table
