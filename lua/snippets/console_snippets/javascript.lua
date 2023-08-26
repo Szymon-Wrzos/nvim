4587cb3a5a55375e8fbd519fb5c38cd60840cd6b
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-08-26 11:32:39 +0200

    Fixes in queries

--- lua/snippets/console_snippets/javascript.lua
+++ lua/snippets/console_snippets/javascript.lua
@@ -1,9 +1,11 @@
 local create_snippet = require("snippets.create_printing_snippet")
 
 local query = [[
-(field_definition (property_identifier) @name (#set! "fetch_last" "true"))
-(function_declaration (identifier) @name (#set! "fetch_last" "true"))
-(variable_declarator (identifier) @name)
+(field_definition (property_identifier) @name (function) (#set! "fetch_last" "true"))
+(field_definition (property_identifier) @name (arrow_function) (#set! "fetch_last" "true"))
+(function_declaration (identifier) @name)
+(variable_declarator (identifier) @name (function) (#set! "fetch_last" "true"))
+(variable_declarator (identifier) @name (arrow_function) (#set! "fetch_last" "true"))
 (method_definition (private_property_identifier) @name)
 (method_definition (property_identifier) @name)
 ]]
