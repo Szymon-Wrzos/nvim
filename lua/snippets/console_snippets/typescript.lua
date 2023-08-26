6f5de93bd3e3cb2e738fe7c01171bab84e00f11e
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-08-26 11:44:13 +0200

    Fixed rest of js snippets

--- lua/snippets/console_snippets/typescript.lua
+++ lua/snippets/console_snippets/typescript.lua
@@ -1,9 +1,12 @@
 local create_snippet = require("snippets.create_printing_snippet")
 
 local query = [[
-(variable_declarator (identifier) @name)
-(public_field_definition (private_property_identifier) @name (#set! "fetch_last" "true"))
-(public_field_definition (property_identifier) @name (#set! "fetch_last" "true"))
+(variable_declarator (identifier) @name (function) (#set! "fetch_last" "true"))
+(variable_declarator (identifier) @name (arrow_function) (#set! "fetch_last" "true"))
+(public_field_definition (private_property_identifier) @name (function) (#set! "fetch_last" "true"))
+(public_field_definition (private_property_identifier) @name (arrow_function) (#set! "fetch_last" "true"))
+(public_field_definition (property_identifier) @name (function) (#set! "fetch_last" "true"))
+(public_field_definition (property_identifier) @name (arrow_function) (#set! "fetch_last" "true"))
 (method_definition (private_property_identifier) @name)
 (method_definition (property_identifier) @name)
 (function_declaration (identifier) @name)
