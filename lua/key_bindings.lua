ed7a81f19dcf218b0279ac1ac05a9ee0a5ed1135
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-10-10 10:55:29 +0200

    Added shortcut for triggering formatting

--- lua/key_bindings.lua
+++ lua/key_bindings.lua
@@ -1,3 +1,4 @@
 local opts = { noremap = true, silent = true }
 
 vim.keymap.set({ "n" }, "<leader>ha", vim.lsp.buf.hover, { desc = "Trigger [h]over [a]ction" })
+vim.keymap.set({ "n" }, "<leader>,", vim.lsp.buf.format, { desc = "Trigger [L]SP formatting" })
