0a7cd1a8150a9419cab6a6803abe224dbf1f206e
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-10-16 09:42:36 +0200

    Changed settings - still doesn't work on my PC

--- lua/plugins/mason/dap.lua
+++ lua/plugins/mason/dap.lua
@@ -73,17 +73,7 @@ M.config = {
 							-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
 							debugger_path = lazypath .. "/vscode-js-debug", -- Path to vscode-js-debug installation.
 							-- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-							adapters = {
-								"chrome",
-								"pwa-node",
-								"pwa-chrome",
-								"pwa-msedge",
-								"node-terminal",
-								"pwa-extensionHost",
-							}, -- which adapters to register in nvim-dap
-							-- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-							-- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-							-- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+							adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
 						})
 						local js_based_languages = { "typescript", "javascript", "typescriptreact" }
 						require("dap.ext.vscode").load_launchjs(nil, {
@@ -117,7 +107,8 @@ M.config = {
 									request = "launch",
 									name = 'Start Chrome with "localhost"',
 									url = "http://localhost:3025",
-									webRoot = "${workspaceFolder}",
+									webRoot = "${workspaceFolder}/src",
+									skipFiles = { "**/node_modules/**/*", "**/@vite/*", "**/src/client/*", "**/src/*" },
 								},
 							}
 						end
