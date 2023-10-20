e277b7317303ccfbf30af5798cd5db4f92ba0b98
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-10-20 15:38:11 +0200

    Wrapped mason_lspconfig loop and init in vim.schedule

--- lua/lazy/config_parts/mason_lspconfig.lua
+++ lua/lazy/config_parts/mason_lspconfig.lua
@@ -14,19 +14,20 @@ local mason_lspconfig = {
 		local mason_lsp = require("mason-lspconfig")
 
 		local langs_table = require("utils.langs_table")
-		local mason_lspconfig_entries = {}
-		for _, data in pairs(langs_table) do
-			for _, lspdata in pairs(data.mason.lspconfig) do
-				table.insert(mason_lspconfig_entries, lspdata)
+		vim.schedule(function()
+			local mason_lspconfig_entries = {}
+			for _, data in pairs(langs_table) do
+				for _, lspdata in pairs(data.mason.lspconfig) do
+					table.insert(mason_lspconfig_entries, lspdata)
+				end
 			end
-		end
 
-		mason_lsp.setup({
-			ensure_installed = mason_lspconfig_entries,
-			automatic_installation = true,
-		})
+			mason_lsp.setup({
+				ensure_installed = mason_lspconfig_entries,
+				automatic_installation = true,
+			})
+		end)
 	end,
-
 	dependencies = {
 		mason,
 	},
