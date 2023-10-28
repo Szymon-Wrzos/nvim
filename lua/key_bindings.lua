024ebc844b98095b12ee5b73e7c7b743f8a0013b
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-10-28 22:59:53 +0200

    Finally shortcut is silent for restarting diagnostics

--- lua/key_bindings.lua
+++ lua/key_bindings.lua
@@ -4,6 +4,6 @@ vim.keymap.set({ "n" }, "<leader>ha", vim.lsp.buf.hover, { desc = "Trigger [h]ov
 vim.keymap.set({ "n" }, "<leader>,", vim.lsp.buf.format, { desc = "Trigger [L]SP formatting" })
 
 vim.keymap.set({ "n" }, "<leader>ef", function()
-	vim.cmd("!eslint_d restart")
+	vim.cmd("silent exec '!eslint_d restart'")
 	vim.diagnostic.reset()
 end, { desc = "Reset eslint_d and diagnostics", silent = true })
