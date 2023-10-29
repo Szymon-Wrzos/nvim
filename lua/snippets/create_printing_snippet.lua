dce45922fe237f65626a97327899321357d161e8
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-10-29 11:23:22 +0100

    Fixed docs

--- lua/snippets/create_printing_snippet.lua
+++ lua/snippets/create_printing_snippet.lua
@@ -10,21 +10,20 @@ local ts = vim.treesitter
 
 local ts_query = ts.query
 
---- @class create_snippet_param
---- @field language string
---- @field format string - format string, should contain {function_name} and {var}. Example:
----```lua
----    [[console.log("[{function_name}]",{var})]]
----```
+---
+--- @class param
+--- @field format string -- format string, should contain {function_name} and {var}.
 --- @field query string - treesitter query for fetching function name
 --- @field breakpoints string[] - breakpoints for treesitter when it should check nearest function name in scope
 --- @field parser_name string | nil - default: `language param` - optional treesitter parser name. Exists due to crashes for jsx and tsx files.
 --- @field program_node_name string - treesitter node name of program (ex. for JS - "program", for Lua - "chunk")
 --- @field trigger string | nil - default: "con" -trigger name of snippet
+--- @field enforce_old_parser boolean | nil - default: false - enforce use old node get
+--- @field language string
 
 --- Function for creating printing snippet for luasnip
 --- In idea, snippet prints printing function for language with name of function in the nearest scope
---- @param param create_snippet_param
+--- @param param param
 local create_printing_snippet = function(param)
 	local function all_trim(str)
 		return str:match("^%s*(.-)%s*$")
@@ -82,16 +81,29 @@ local create_printing_snippet = function(param)
 		return final_text
 	end
 
+	--- Since nvim 0.10.0, tsx and jsx files are properly handled
+	local function get_node(snip)
+		local current_version = vim.version()
+		local current_version_table = { current_version.major, current_version.minor, current_version.patch }
+		local version_with_fixed_tsx = { 0, 10, 0 }
+		local is_current_version_fixed_tsx = vim.version.cmp(current_version_table, version_with_fixed_tsx) >= 0
+		if is_current_version_fixed_tsx and not param.enforce_old_parser then
+			return ts:get_node()
+		else
+			local pos_begin = snip.nodes[1].mark:pos_begin()
+			local pos_end = snip.nodes[1].mark:pos_end()
+			local parser = vim.treesitter.get_parser(0, param.parser_name or param.language)
+			local tstree = parser:parse()
+
+			return tstree[1]:root():named_descendant_for_range(pos_begin[1], pos_begin[2], pos_end[1], pos_end[2])
+		end
+	end
+
 	local console = sn(
 		param.trigger or "con",
 		fmt(param.format, {
 			function_name = d(1, function(args, snip)
-				local pos_begin = snip.nodes[1].mark:pos_begin()
-				local pos_end = snip.nodes[1].mark:pos_end()
-				local parser = vim.treesitter.get_parser(0, param.parser_name or param.language)
-				local tstree = parser:parse()
-				local ts_node =
-					tstree[1]:root():named_descendant_for_range(pos_begin[1], pos_begin[2], pos_end[1], pos_end[2])
+				local ts_node = get_node(snip)
 				local checked_value = all_trim(args[1][1])
 				local function_root = seek_function_root(ts_node, param.breakpoints)
 				local data = function_root ~= nil and get_node_text(function_root) or ""
